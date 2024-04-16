d2a9a0f44245191c2db4d7e852d3c781bc308152
Yassine Botza LOCAL <yassinebotza10@gmail.com>
2024-04-16 20:35:21 +0200

    yassine1 code done ! :)

---
+++ yassine1.c
@@ -0,0 +1,106 @@
+#include<stdio.h>
+
+void main(){
+
+	printf("this is my first script\n");
+	
+	printf("\t1");
+	
+	printf("\n\t2\n");
+		 
+	printf("so this is what I learned today\n");
+	
+	printf("Ex:1\n");
+			
+	printf("* * * *\n");
+	
+	printf("* * * *\n");
+	
+	printf("* * * *\n");
+	
+	printf("* * * *\n");
+	
+	printf("space\n");
+	
+	printf("Ex:2\n");
+		
+	printf("*\n");
+	
+	printf("* *\n");
+	
+	printf("* * *\n");
+	
+	printf("* * * *\n");
+	
+	
+	// ex 3 ---------------------------------
+	printf("Ex:3\n");
+	
+	printf("* * * *\n");
+	
+	printf("  * * *\n");
+	
+	printf("    * * \n");
+	
+	printf("      *\n");
+		
+		//it s so funny
+	printf("Ex:4\n");
+		
+	printf("    *  \n");
+	
+	printf("  * * *\n");
+	
+	printf("* * * * *\n");
+	
+	printf("Ex:5\n");
+	
+	printf("*\n");
+	
+	printf("* *\n");
+	
+	printf("* * *\n");
+	
+	printf("* * * *\n");
+	
+	printf("* * *\n");
+	
+	printf("* *\n");
+	
+	printf("*\n");
+	
+	printf("Ex:6\n");
+	
+	printf("  *       *  \n");
+	
+	printf("* * *   * * *\n");
+	
+	printf("* * * * * * *\n");
+	
+	printf("  * * * * *  \n");
+	
+	printf("    * * *    \n");
+	
+	printf("      *      \n");
+	
+	printf("Ex:8\n");
+	
+	printf("__________ \n");
+	
+	printf("|        |\n");
+	
+	printf("| *    * |\n");
+	
+	printf("|   4    |\n");
+	
+	printf("| |    | |\n");
+	
+	printf("| ------ |\n");
+	
+	printf("|________|\n");
+	
+	printf("Ex:9\n");
+	
+	printf("*  ");
+		
+		}
