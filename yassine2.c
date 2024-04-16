d88d9626b87e1a8c8eea0a28e2f29f0d26a39375
Yassine Botza LOCAL <yassinebotza10@gmail.com>
2024-04-16 20:45:39 +0200

    yassine2 code done

---
+++ yassine2.c
@@ -0,0 +1,90 @@
+#include<stdio.h>
+
+
+void main() 
+{	
+
+// we create a variable of type int; VAR DECLARATION	
+int a;
+int b;
+int result;
+
+// Then the machine print the sentence and we can jump the the line with bake slash + n;
+printf("Please give me the first number: \n");
+printf("Ok?\n");
+
+
+
+// After that , the machine store (input) it in a ; var initiation
+scanf("%d",&a);
+
+printf(" a = %d\n",a);
+
+printf("please give me the second number: \n");
+
+scanf("%d",&b);
+
+printf("b =%d\n",b);
+
+printf("____________________________\n");
+
+result=a+b;
+
+printf(" ok\n");
+
+printf("this equals \n");
+
+printf("%d+%d=%d \n ",a ,b ,result);
+ 
+printf(" So now we're gonna move to something else\n");
+
+printf("_____________________________________________________\n");
+
+printf("give me the first number : \n");
+
+scanf("%d" ,&a);
+
+printf("a=%d\n",a);
+
+printf("give me the second number: \n");
+
+scanf("%d",&b);
+
+printf("b=%d\n",b);
+
+result=a*b;
+
+printf("%d*%d=%d \n", a, b, result);
+
+printf("________________________________________________________________________________\n");
+
+printf("                                 S P A C E      \n ");
+
+printf("Plea.se give me the first number");
+
+scanf("%d",&a);
+
+printf("a=%d\n",a);
+
+printf("please give the second number\n");
+
+scanf("%d",&b);
+
+printf("c=%d\n",b);
+
+
+result=a-b;
+
+printf("%d-%d=%d \n",a ,b ,result); 
+
+printf("thanks for using our program");
+
+//
+
+
+
+
+
+	
+	
+}
