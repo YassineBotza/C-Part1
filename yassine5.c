fe891a81472b12001cb6063e2af1050933c7028c
Yassine Botza LOCAL <yassinebotza10@gmail.com>
2024-04-16 21:08:19 +0200

    yassine 5 code done

---
+++ yassine5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void main ()
+{
+
+int a [3];
+	
+printf("plaese,give me a number: ");
+scanf("%d",&a[0]);
+printf("%d\n",a[0]);
+
+printf("plaese,give me the second number: ");
+scanf("%d",&a[1]);
+printf("%d\n",a[1]);
+
+printf("plaese,give me the third number: ");
+scanf("%d",&a[2]);
+printf("%d\n",a[2]);
+ 
+	
+}
