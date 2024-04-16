a5e4cac9a78cd04425f31fb24e27770f43360610
Yassine Botza LOCAL <yassinebotza10@gmail.com>
2024-04-16 21:10:04 +0200

    yassine 6 code done

---
+++ yassine6.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>	
+void main(){
+		
+  char Hi[50];
+  printf(" You have to write SALAM \n");
+  printf(" Ok? :");
+  scanf("%s",&Hi);
+  getchar();  
+  printf("thanks for your help you just wrote :%s ",Hi);
+ 		
+
+	}
