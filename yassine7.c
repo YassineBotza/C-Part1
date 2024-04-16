d34f688d41ebadbe9899631f23874f6b1748b9d9
Yassine Botza LOCAL <yassinebotza10@gmail.com>
2024-04-16 21:11:20 +0200

    yassine 7 code done

---
+++ yassine7.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+  
+  void main (){
+  	char Hi[50];
+    	printf("\t\tHi humain\n");
+     	printf("\t\twho are you\n");
+    	printf("\t\tcan I have your name , please !\n");
+    	printf("\t\t\t\tOK ? :",Hi);
+        scanf("%s",&Hi);
+        getchar();  
+        
+        // You have to see the result____________________________________//
+        
+        printf("\t\tOK \n");
+        printf("\t\thi mr %s ",Hi);
+  	
+  }
