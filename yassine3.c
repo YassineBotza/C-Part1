ddab1ea40f97b5a428c029d9939aa0fcb35fa516
Yassine Botza LOCAL <yassinebotza10@gmail.com>
2024-04-16 20:57:21 +0200

    yassine 3 code done

---
+++ yassine3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void main () 
+{
+	
+float a;
+
+printf(" Give me the first number :  ",a);
+
+scanf("%f" ,&a);
+
+printf("a equals %f \n",a);
+	
+	if(a>=0)
+	{	
+		printf("a equals %f and  %f is positif",a ,a);	
+	
+	}else
+	{
+		printf("a equals %f and %f is negatif",a ,a);
+	}
+	
+}  
