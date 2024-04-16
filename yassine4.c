3fcfe6efcf631ad0f45eaef7ee51cac2e147efac
Yassine Botza LOCAL <yassinebotza10@gmail.com>
2024-04-16 21:00:05 +0200

    yassine 4 code done

---
+++ yassine4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+void main()  
+{	
+ char you;
+ scanf("%c",&you);
+ printf("%c\n",you);
+ 
+ printf("_______________space________________\n");
+ 
+ printf("%d\n",you+1);
+ printf("%c",you+1);
+ 
+ 
+ 
+}
